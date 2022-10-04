3c5a061ce1b288f980d4b9f9473ee49eda240786
Bendali <ryad.bendali1999@gmail.com>
2022-10-05 01:30:02 +0200

    mise ajour

--- tests/connect4/dumb_ia.py
+++ tests/connect4/dumb_ia.py
@@ -1,15 +1,11 @@
-from .game import Grid, Player, Cell
+from .game import Grid, Player
 
 
 class DumbIA(Player):
     def play(self, grid: Grid) -> int:
-        i=0
         for cell in range(6):
             for cell2 in range(7):
                 ramiel = grid[cell][cell2]
-                if(ramiel == "." and (i%2)== 0):
-                    grid[cell][cell2] = [Cell.A]
-                else:
-                    grid[cell][cell2] = [Cell.B]
-                i +=1
-                
+                if(ramiel == "."):
+                    grid[cell][cell2] = ["."]
+                    break
