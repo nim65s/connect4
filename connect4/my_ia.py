8c00c55296e342b95955fea35e89396b776b1bd2
Ahcene BARA <bara00ahcene@gmail.com>
2022-11-16 10:29:44 +0100

    test de mise à jour

---
+++ connect4/my_ia.py
@@ -0,0 +1,14 @@
+from .game import Player
+from .game import Cell
+
+
+class DumbIA(Player):
+    def play(self, grid) -> int:
+        for line in range(grid.lines):# on parcoure les lignes de la grille
+            for colonne in range(grid.columns):#on parcoure les colonnes de la grille
+                if grid.grid[line][colonne] == Cell.A:#on teste si la case qui correspond à la ligne et la colonne est vide
+                    print("Cette case est vide, je vais jouer ici :)")#on dit que c'est bon
+                    return colonne#on retourne la premiere colonne dans laquelle on peut mettre un jeton
+
+
+
